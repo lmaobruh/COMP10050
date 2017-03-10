8932e3161474834a57220b3768e839cd05c951e2
lmaobruh <aayan.atiq@ucdconnect.ie>
2017-03-10 23:57:12 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,111 @@
+/* Software Engineering Assignment 2 by Aayan Atiq (16203115)
+Note: Since my group partner dropped out, this program only has limited capabilities.
+There are no functionalities related to 'slots' or there implementation in this program.
+This is as per instructions given by Dr. Liliana Pasquale */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void PlayerSet(int i);
+void Attack(int i);
+
+typedef struct Player {
+    float life;
+    int smart;
+    int magic;
+    int strength;
+    int dexterity;
+    int luck;
+    char playerType[10];
+    char playerName[50];
+}Player;
+    //this struct stores and keeps track of each player's info
+
+int main() {
+    int n;
+    printf("Enter Number of Players (1 - 6): ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        PlayerSet(i);
+    }
+    /* Since I was asked not to include slots, the players are not
+    given options asking them what to do. Instead they just attacl */
+    for (int i = 0; i < n; i++) {
+        Attack(i);
+    }
+    return 0;
+}
+
+void PlayerSet(int i) {
+    Player *P;
+    P = (int*)malloc(n * sizeof(P));
+    printf("Enter Player %d Type:\nA: Elf\nB: Human\nC: Ogre\nD: Wizard\n", i+1);
+    char choice = getchar();
+    if (c == 'a' || c == 'A') strcpy(P[i].type , Elf);
+
+    if(c == 'b' || c == 'B') strcpy(P[i].type , Human);
+
+    if(c == 'c' || c == 'C') strcpy(P[i].type , Ogre);
+
+    if(c == 'd' || c == 'D') strcpy(P[i].type , Wizard);
+        //player type
+
+    printf("Enter Player %d Name: ", i+1);
+    scanf("%s", P[i].name);
+
+    P[i].life = 100.00;
+        //initializing life points to 100
+    int total;
+
+    if(strcmp(Human , P[i].type) == 0) {
+
+        do {
+            P[i].smart = 1 + rand()%100;
+            P[i].strength = 1 + rand()%(100);
+            P[i].magic = 1 + rand()%(100);
+            P[i].luck = 1 + rand()%(100);
+            P[i].dexterity = 1 + rand()%(100);
+            total = P[i].smart + P[i].strength + P[i].magic + P[i].luck + P[i].dexterity;
+
+        } while(total > 299);
+    }
+
+    if(strcmp(Ogre , P[i].type) == 0) {
+
+        P[i].magic = 0;
+        P[i].strength=  rand()%(100 +1 -80)+80 ;
+        P[i].dexterity= rand()%(100 +1 -80)+80;
+
+        do {
+            P[i].smart=  rand()%20;
+            P[i].luck= rand()%50;
+            n= P[i].smart + (int)P[i].luck;
+        } while(n > 49);
+    }
+
+    if(strcmp(Elf, P[i].type)==0) {
+
+        P[i].luck= rand()%(100 +1 -60)+60;
+        P[i].smart= rand()%(100 +1 -70)+7;
+        P[i].strength= rand()%(50 +1 -1)+1;
+        P[i].magic = rand()%(79 +1 -51)+51;
+        P[i].dexterity=rand()%100 +1;
+    }
+
+    if(strcmp (Wizard, P[i].type)==0) {
+
+        P[i].luck= rand()%(100 +1 -50)+50;
+        P[i].smart=rand()%(100 +1 -90)+90;
+        P[i].strength= rand()%(20 +1 -1)+1;
+        P[i].magic=rand()%(100 +1 -80)+1;
+        P[i].dexterity= rand()%100;
+    }
+}
+
+void Attack(int i) {
+    /* Since there are no slots, I'm going to provide code on what to do
+    if there are either one or two players nearest to the attacker */
+    
+}
